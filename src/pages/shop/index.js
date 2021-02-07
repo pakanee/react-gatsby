4d920985656ecf88e69cd5458e5bd155f40e9623
pakanee <pjoforproject1@gmail.com>
2021-02-07 12:15:56 +0700

    update data map

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react'
+import React from 'react'
 import  {Router} from '@reach/router'
 import Axios from 'axios'
 
 import Layout from '../../components/layout';
 
- class Shop extends Component {
+ class Shop extends React.Component {
     constructor(){
         super();
         console.log('constructor');
         this.state={
-            is_loading:false,
+            is_loading:true,
+            data:[],
         };
 
     }
     componentDidMount(){
         console.log('componentDidMount');
         // เรียก API จังหวะนี้
+        const url= 'http://localhost:1337/shops';
+        const config ={
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMTYzYzE0MDViNWViMzQzYzhlNGI5ZCIsImlhdCI6MTYxMjA3NjM2OCwiZXhwIjoxNjE0NjY4MzY4fQ.u6kNn09dAe4M4ysJ2yUraIHWDCvjkdf_a6dnWa5Yokc'
+        };
+
+        Axios.get(url,config)
+        .then(response => {
+            console.log('shop data', response.data);
+            if(response.data){
+                this.setState({
+                    is_loading:false,
+                    data: response.data,
+                });
+            }
+        })
+        .catch(err=>{
+           // console.log('shop error',err);
+        });
     }
-    componentWillMount(){
-        console.log('componentWillMount');
+    UNSAFE_componentWillMount(){
+       // console.log('componentWillMount');
         // ใส่ spin แสดงการโหลดข้อมูล
     }
 
     render() {
         console.log('render');
-        if(!this.state.is_loading){
+        if(this.state.is_loading){
            return  <Layout>Loading...</Layout>
         }
+       // console.log(this.state.data);
         return (
             <Layout>
-                Shop/Index
+            Shop List
+                {this.state.data.map(item =>
+                     <div key={item._id}>
+                     id : {item._id}
+                        <div> name : {item.name}</div>
+                     </div>
+                )}
             </Layout>
         )
     }
