6c762a08c3323a8436b02fd9c0e468d232380e92
pakanee <pjoforproject1@gmail.com>
2021-02-07 10:57:10 +0700

    init commit

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+// import
+import React from "react";
+import Layout from '../components/layout'
+
+//Component
+ const About =() => <Layout>About</Layout>
+
+//export
+export default About;
---
+++ src/pages/shop/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import  {Router} from '@reach/router'
+
+import Layout from '../../components/layout';
+
+ class Shop extends Component {
+    constructor(){
+        super();
+        console.log('constructor');
+        this.state={
+            is_loading:false,
+        };
+
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+        // เรียก API จังหวะนี้
+    }
+    componentWillMount(){
+        console.log('componentWillMount');
+        // ใส่ spin แสดงการโหลดข้อมูล
+    }
+
+    render() {
+        console.log('render');
+        if(!this.state.is_loading){
+           return  <Layout>Loading...</Layout>
+        }
+        return (
+            <Layout>
+                Shop/Index
+            </Layout>
+        )
+    }
+}
+export default Shop;
